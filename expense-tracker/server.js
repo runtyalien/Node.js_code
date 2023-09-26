b739b0bb57f0ed85b5dfd1e9a3c74d1a63def1f1
runtyalien <omgadkar@gmail.com>
2023-09-26 13:07:19 +0530

    email in use

--- expense-tracker/server.js
+++ expense-tracker/server.js
@@ -5,69 +5,73 @@ const Sequelize = require("sequelize");
 const app = express();
 const port = 3000;
 
-const sequelize = new Sequelize("user-expense", "root", "omkar", {
-    host: "localhost",
-    dialect: "mysql",
-    define: {
-        timestamps: true,
-        underscored: true,
-    },
+const sequelize = new Sequelize("usersexpense", "root", "omkar", {
+  host: "localhost",
+  dialect: "mysql",
 });
 
-const usersexpense = sequelize.define("usersexpense", {
+const User = sequelize.define(
+  "userexpenses",
+  {
     id: {
-        type:Sequelize.INTEGER,
-        primaryKey: true,
-        allowNull:false,
-        unique:true,
-        autoIncrement: true,
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
     },
     name: {
-        type:Sequelize.STRING,
-        allowNull:false,
+      type: Sequelize.STRING,
+      allowNull: false,
     },
     email: {
-        type:Sequelize.STRING,
-        allowNull:false,
-        unique:true,
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true,
     },
     password: {
-        type:Sequelize.STRING,
-        allowNull:false,
-    }, 
-})
-
-
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    timestamps: false,
+  }
+);
 
 app.use(bodyParser.json());
 
 //app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/views/signup.html");
+  res.sendFile(__dirname + "/views/signup.html");
+});
+
+sequelize
+  .sync()
+  .then(() => {
+    console.log("Database is synced");
+    app.listen(port, () => {
+      console.log(`Server listening on ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Error syncing database", err);
   });
 
-sequelize.sync()
-    .then(() => {
-        console.log("Database is synced");
-        app.listen(port, () => {
-            console.log(`Server listening on ${port}`);
-        });
-    })
-    .catch((err) => {
-        console.error("Error syncing database", err);
-    });
-
-
-app.post("/signup", async(req, res) => {
+  app.post("/signup", async (req, res) => {
     const { name, email, password } = req.body;
 
     try {
-        const user = await usersexpense.create({ name, email, password });
+        const user = await User.create({ name, email, password });
         console.log("User registered", user.email);
-        res.status(200).json({ message: "Signup successful"});
+        res.status(200).json({ message: "Signup successful" });
     } catch (error) {
-        console.error("Error",error);
-        res.status(500).json({error: "Sign up failed"});
+        if (error.name === 'SequelizeUniqueConstraintError') {
+            // This error occurs when the email is already in use.
+            res.status(400).json({ error: "Email already in use" });
+        } else {
+            console.error("Error", error);
+            res.status(500).json({ error: "Sign up failed" });
+        }
     }
 });
