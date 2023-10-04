228eff645cced541fb3b2f024ec02e2a6e9ffff8
runtyalien <omgadkar@gmail.com>
2023-10-05 01:41:50 +0530

    Leaderboard Optimization

--- expense-tracker/controller/premium.js
+++ expense-tracker/controller/premium.js
@@ -1,43 +1,33 @@
-const User = require('../model/user');
-const Expense = require('../model/expense');
+const User = require("../model/user");
+const Expense = require("../model/expense");
+const sequelize = require("../utils/database");
 
 async function showleaderboard(req, res) {
     try {
-      // Fetch all users
-      const users = await User.findAll();
+      // Fetch all users with total expenses
+      const usersWithTotalExpenses = await User.findAll({
+        attributes: [
+          'name',
+          [sequelize.fn('SUM', sequelize.col('Expenses.amount')), 'totalExpenses'],
+        ],
+        include: [
+          {
+            model: Expense,
+            attributes: [],
+          },
+        ],
+        group: ['userexpenses.id'], 
+        order: [['totalExpenses', 'DESC']],
+      });
   
-      // Calculate total expenses for each user
-      const usersWithTotalExpenses = await Promise.all(
-        users.map(async (user) => {
-          if (user.id !== undefined && user.id !== null) {
-            const totalExpenses = await Expense.sum('amount', {
-              where: { userexpenseId: user.id },
-            });
-      
-            console.log(user.id);
-            console.log(totalExpenses);
-      
-            return {
-              username: user.name,
-              totalExpenses,
-            };
-          } else {
-            console.log(`user.userexpenseId is not defined for ${user.name} ${user.id}`);
-            return null;
-          }
-        })
-      );
-      
-      // Filter out null entries
-      const filteredUsersWithTotalExpenses = usersWithTotalExpenses.filter(Boolean);
-      
-      // Sort and respond
-      filteredUsersWithTotalExpenses.sort((a, b) => b.totalExpenses - a.totalExpenses);
-      res.status(200).json(filteredUsersWithTotalExpenses);
+      usersWithTotalExpenses.forEach(user => {
+        console.log(`Name: ${user.name}, Total Expenses: ${user.dataValues.totalExpenses}`);
+      });
+      res.status(200).json(usersWithTotalExpenses);
     } catch (error) {
       console.error(error);
       res.status(500).json({ message: 'Something went wrong' });
     }
   }
-
-module.exports = { showleaderboard };
+  
+  module.exports = { showleaderboard };
